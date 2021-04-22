316afdbb45e8d1861cf53722a18d6c0fa7976a75
Pulkit Chaudhary <pulkithb@gmail.com>
2021-04-22 17:01:26 +0530

    Database updated

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const knex = require('knex');
 const db = knex({
 	client: 'pg',
 	connection:{
-		host: '127.0.0.1',
-		user: 'postgres',
-		password: 'test',
-		database: 'postgres'
+		client: 'pg',
+	connection:{
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
+	}
 	}
 }); 
 const app = express();
