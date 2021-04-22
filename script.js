f1940aaea95632b3c62c108f915895c0d874b11e
Pulkit Chaudhary <pulkithb@gmail.com>
2021-04-22 17:07:01 +0530

    Database error corrected

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const knex = require('knex');
 const db = knex({
 	client: 'pg',
 	connection:{
-		client: 'pg',
-	connection:{
+		
 		connectionString: process.env.DATABASE_URL,
 		ssl: true
-	}
+	
 	}
 }); 
 const app = express();
