9179aeca891ed31f4b9c10db3143df261894463d
Pulkit Chaudhary <pulkithb@gmail.com>
2021-04-22 22:31:58 +0530

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const knex = require('knex');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
 const db = knex({
@@ -12,7 +12,7 @@ const db = knex({
 }); 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+//app.use(cors())
 /*app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
@@ -23,7 +23,7 @@ app.use(cors())
 });*/
 //console.log(db.select('*').from('favourite'))
 app.post('/addfav',(req,res) => {
-	res.set('Access-Control-Allow-Origin', '*');
+	//res.set('Access-Control-Allow-Origin', '*');
 	db('favourite').returning('*').insert({
 		email:req.body.email,
 		id: req.body.id,
@@ -33,31 +33,31 @@ app.post('/addfav',(req,res) => {
 		description: req.body.title
 	})
 	.then(response=>{
-		res.set('Access-Control-Allow-Origin', '*');
+		//res.set('Access-Control-Allow-Origin', '*');
 		res.json(response[0])
 	})
 	.catch(err=> res.status(400).json('Unable to add to favourite'))
 })
 app.get('/getfav/:email',(req,res) => {
-	res.set('Access-Control-Allow-Origin', '*');
+	//res.set('Access-Control-Allow-Origin', '*');
 	const {email}= req.params
 	
 	db.select('*').from('favourite').where('email','=',email)
 	.then(response => {
-		res.set('Access-Control-Allow-Origin', '*');
+		//res.set('Access-Control-Allow-Origin', '*');
 		res.json(response)
 	})
 	.catch(err=> res.status(400).json('Unable to load favourite'))
 
 })
 app.post('/delfav',(req,res) => {
-	res.set('Access-Control-Allow-Origin', '*');
+	//res.set('Access-Control-Allow-Origin', '*');
 	db('favourite').where(
 		'email','=',req.body.email
 		
 	).where('id','=',req.body.id).del()
 	.then(response => {
-		res.set('Access-Control-Allow-Origin', '*');
+		//res.set('Access-Control-Allow-Origin', '*');
 		res.json(response)
 	})
 	.catch(err=> res.status(400).json('Unable to delete Favourite'))
